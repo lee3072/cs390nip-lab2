f8d43dd598df25767544739718e03821e8ceaad1
lee3072 <lee3072@purdue.edu>
2020-10-05 12:59:36 -0400

    GetRawData() setup first commit

--- Lab1.py
+++ Lab1.py
@@ -87,11 +87,14 @@ def getRawData():
         mnist = tf.keras.datasets.fashion_mnist
         (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
     elif DATASET == "cifar_10":
-        pass      # TODO: Add this case.
+        mnist = tf.keras.datasets.cifar10.load_data()
+        (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
     elif DATASET == "cifar_100_f":
-        pass      # TODO: Add this case.
+        mnist = tf.keras.datasets.cifar100.load_data(label_mode="fine")
+        (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
     elif DATASET == "cifar_100_c":
-        pass      # TODO: Add this case.
+        mnist = tf.keras.datasets.cifar100.load_data(label_mode="coarse")
+        (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
     else:
         raise ValueError("Dataset not recognized.")
     print("Dataset: %s" % DATASET)
